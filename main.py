9a9982195b342aa81d573498a2c6471c4bdcf172
Filipe Keppe <fikeppe@gmail.com>
2022-03-20 20:42:46 -0300

    Armazenando o conteúdo html da página e as divs com as informaçõesque vou usar

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import requests
 url= "https://www.vultr.com/pricing/#cloud-compute/"
 page = requests.get(url)
 
+content = BeautifulSoup(page.content, 'html.parser')
+contentInfo = content.find_all('div', class_="pt__row-content")
+
 
 
 

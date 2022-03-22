9f4d103ff9708ba2267bfe9915c08cf1524eb34e
Filipe Keppe <fikeppe@gmail.com>
2022-03-22 20:51:23 -0300

    Criando um loop para encontrar cada item das tags que contém as informações que vou usar.

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ page = requests.get(url)
 content = BeautifulSoup(page.content, 'html.parser')
 contentInfo = content.find_all('div', class_="pt__row-content")
 
+for list in contentInfo:
+    eachInfo = list.find('div', class_="pt__cell js-price")
+    price = list.find('div', class_="pt__cell js-price pt__cell--price pt__cell-price")
+    allInfo = [eachInfo, price]
+    print(allInfo)
+    
+
+
+
 
 
 

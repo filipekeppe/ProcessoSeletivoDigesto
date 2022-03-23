9ee2afdf7cd9141a3ec5ef08a1bc48ec1967076c
Filipe Keppe <fikeppe@gmail.com>
2022-03-22 21:49:34 -0300

    Convertendo os dados html em texto e limpando o mesmo para printar de forma mais clara.

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ url= "https://www.vultr.com/pricing/#cloud-compute/"
 page = requests.get(url)
 
 content = BeautifulSoup(page.content, 'html.parser')
-contentInfo = content.find_all('div', class_="pt__row-content")
+contentInfo = content.find_all('div', class_="pt__row")
 
 for list in contentInfo:
-    eachInfo = list.find('div', class_="pt__cell js-price")
-    price = list.find('div', class_="pt__cell js-price pt__cell--price pt__cell-price")
-    allInfo = [eachInfo, price]
+    eachInfo = list.find('div', class_="pt__cell js-price").text.replace('\xa0', '')
+    memory = list.find('span', class_="is-hidden-lg-up").text.replace('\xa0', '')
+    price = list.find('div', class_="pt__cell js-price pt__cell--price pt__cell-price").text.replace('\xa0', '')
+    allInfo = [eachInfo, memory, price]
     print(allInfo)
-    
+
 
 
 
